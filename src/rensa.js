5f10825ca6af2d4a725014730c211787b5b74a3c
haruironoinochi <117558266+haruironoinochi@users.noreply.github.com>
2023-08-08 20:45:32 +0900

    Update rensa.js

--- src/rensa.js
+++ src/rensa.js
@@ -35,21 +35,21 @@ class Score {
  score = Math.floor(score / 10);
  }
  }
- //static calculateScore (rensa, piece, color) {
- //rensa = Math.min(rensa, Score.rensaBonus.length - 1);
- //piece = Math.min(piece, Score.pieceBonus.length - 1);
- //color = Math.min(color, Score.colorBonus.length - 1);
- //let scale = Score.rensaBonus[rensa] + Score.pieceBonus[piece] + Score.colorBonus[color];
- //if(scale === 0) {
- //scale = 1;
- //}
- //this.addScore(scale * piece * 10);
- //}
+ static calculateScore (rensa, piece, color) {
+ rensa = Math.min(rensa, Score.rensaBonus.length - 1);
+ piece = Math.min(piece, Score.pieceBonus.length - 1);
+ color = Math.min(color, Score.colorBonus.length - 1);
+ let scale = Score.rensaBonus[rensa] + Score.pieceBonus[piece] + Score.colorBonus[color];
+ if(scale === 0) {
+ scale = 1;
+ }
+ this.addScore(scale * piece * 10);
+ }
  static addScore (score) {
  this.rensa += 1;
   this.showRensa();
   }
  };
- //Score.rensaBonus = [0, 8, 16, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512, 544, 576, 608, 640, 672];
- //Score.pieceBonus = [0, 0, 0, 0, 2, 3, 4, 5, 6, 7, 10, 10];
- //Score.colorBonus = [0, 0, 3, 6, 12, 24];
+ Rensa.rensaBonus = [0, 8, 16, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512, 544, 576, 608, 640, 672];
+ Rensa.pieceBonus = [0, 0, 0, 0, 2, 3, 4, 5, 6, 7, 10, 10];
+ Rensa.colorBonus = [0, 0, 3, 6, 12, 24];
